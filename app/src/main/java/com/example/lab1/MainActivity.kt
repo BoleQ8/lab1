dc8a35b9810ec6a7fc89aeb44eef6984fed7ff06
BoleQ8 <boleq7@gmail.com>
2021-03-24 18:52:20 +0100

    dodane metody .error oraz zmieniłem w setOnFocusChangeListener nazwisko itp

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -24,22 +24,25 @@ class MainActivity : AppCompatActivity() {
                 //Toast.makeText(applicationContext, "Jest fokus", Toast.LENGTH_SHORT).show()
             } else if(!hasFocus && imie.text.isEmpty()) {
                 Toast.makeText(applicationContext, "Pole imie nie moze byc puste", Toast.LENGTH_SHORT).show()
+                imie.error="Imie nie może być puste!"
             }
         }
 
         nazwisko.setOnFocusChangeListener { _, hasFocus ->
             if(hasFocus) {
                 //Toast.makeText(applicationContext, "Jest fokus", Toast.LENGTH_SHORT).show()
-            } else if(!hasFocus && imie.text.isEmpty()) {
+            } else if(!hasFocus && nazwisko.text.isEmpty()) {
                 Toast.makeText(applicationContext, "Pole nazwisko nie moze byc puste", Toast.LENGTH_SHORT).show()
+                nazwisko.error="Nazwisko nie może być puste!"
             }
         }
 
         liczbaOcen.setOnFocusChangeListener { _, hasFocus ->
             if(hasFocus) {
                 //Toast.makeText(applicationContext, "Jest fokus", Toast.LENGTH_SHORT).show()
-            } else if(!hasFocus && imie.text.isEmpty()) {
+            } else if(!hasFocus && liczbaOcen.text.isEmpty()) {
                 Toast.makeText(applicationContext, "Pole liczba ocen nie moze byc puste", Toast.LENGTH_SHORT).show()
+                liczbaOcen.error="Podaj oceny od 5 do 15!"
             }
         }
 
@@ -49,7 +52,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if(imie.text.toString().isEmpty() || nazwisko.text.toString().isEmpty() || liczbaOcen.text.toString().isEmpty()) {
+                if(imie.text.toString().isEmpty() || nazwisko.text.toString().isEmpty() || liczbaOcen.text.isEmpty()) {
                     przyciskOceny.visibility = Button.INVISIBLE
                 } else {
                     przyciskOceny.visibility = Button.VISIBLE
